69d0027915c9bea57e85bf4aab544552b65234f9
JonasJenn <104638647+JonasJenn@users.noreply.github.com>
2022-09-20 12:27:33 +0100

    Update Script file for COVID19 Europe.sql
    Exploring deaths and infection counts due to Covid-19 in Europe

--- Script file for COVID19 Europe.sql
+++ Script file for COVID19 Europe.sql
@@ -1,3 +1,4 @@
+-- Create table for csv file
 CREATE TABLE COVID_19Deaths(
 Date_Reported text,
 Country_code varchar(50),
@@ -8,26 +9,27 @@ total_cases int,
 total_deaths int
 );
 
+-- Upload csv file into SQL
 LOAD DATA INFILE 'C:\COVIDDataforEurope.csv'
 INTO TABLE COVID_19deaths
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
+-- Explore table 
 SELECT * 
 FROM covid_19deaths;
 
--- Looking at COVID-19 data for Countries in Europe
--- Find the Death Percentage
+-- Find the Death Percentage in each country
 SELECT date_reported, Location, total_cases, Total_deaths, (total_deaths/total_cases)*100 AS Death_Percentage
 FROM covid_19deaths
 ORDER BY 1,2;
 
--- Percentage of Population with Covid
+-- Percentage of Population with Covid in each country
 SELECT date_reported, Location, total_cases, population2020, (total_cases/population2020)*100 AS Percentage_of_population_Infected
 FROM covid_19deaths
 ORDER BY location;
 
--- Countries with the Highest Infection Rate compared to Population
+-- Countries with the Highest Infection Count compared to Population
 SELECT Location, MAX(total_cases) AS Highest_Infection_Count, population2020, ((MAX(total_cases)/population2020))*100 AS Percentage_of_population_Infected
 FROM covid_19deaths
 GROUP BY location, population2020
@@ -38,6 +40,8 @@ SELECT Location, MAX(total_deaths) AS Highest_Death_Count
 FROM covid_19deaths
 GROUP BY location
 ORDER BY Highest_Death_Count desc;
+
+-- Creating views for visualizations in Tableau
 
 Create View Death_Percentage AS
 SELECT date_reported, Location, total_cases, Total_deaths, (total_deaths/total_cases)*100 AS Death_Percentage
